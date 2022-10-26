d98543804b2d363052cb2badb680c2b6e2c96f38
hovik <krishovi@hiof.no>
2022-10-26 15:03:14 +0200

    Changed name on test file

--- src/test/java/Test_leapYear.java
+++ src/test/java/Test_leapYear.java
@@ -2,28 +2,28 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class leapYearTest {
+class Test_leapYear {
     @Test
-    void yearDivisibleByFourButNotHundred() { //Tester at året er delbart med 4 men ikke 100.
+    void TestYearDivisibleByFourButNotHundred() { //Tester at året er delbart med 4 men ikke 100.
         leapYear leapYear = new leapYear();
         assertTrue(leapYear.isLeapYear(4));
         assertFalse(leapYear.isLeapYear(100));
     }
     @Test
-    void yeardivisibleByFourHundred() { //Tester at året er delbart med 400.
+    void TestYeardivisibleByFourHundred() { //Tester at året er delbart med 400.
         leapYear leapYear = new leapYear();
         assertTrue(leapYear.isLeapYear(400));
         assertTrue(leapYear.isLeapYear(800));
     }
     @Test
-    void yeardivisibleByFour() { //Tester at året er delbart med 4.
+    void TestYeardivisibleByFour() { //Tester at året er delbart med 4.
         leapYear leapYear = new leapYear();
         assertTrue(leapYear.isLeapYear(4));
         assertTrue(leapYear.isLeapYear(8));
 
     }
     @Test
-    void yeardivisibleByFourHundredButNotHundred() { //Tester at året er delbart med 400, men ikke 100.
+    void TestYeardivisibleByFourHundredButNotHundred() { //Tester at året er delbart med 400, men ikke 100.
         leapYear leapYear = new leapYear();
         assertTrue(leapYear.isLeapYear(400));
         assertFalse(leapYear.isLeapYear(100));
