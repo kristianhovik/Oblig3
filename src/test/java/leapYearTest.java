a84a208a2bb0c197180e4dd3474ab77115b9742f
hovik <krishovi@hiof.no>
2022-10-26 14:04:39 +0200

    change maven sourceasdasd

--- src/test/java/leapYearTest.java
+++ src/test/java/leapYearTest.java
@@ -5,26 +5,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class leapYearTest {
     @Test
     void yearDivisibleByFourButNotHundred() { //Tester at året er delbart med 4 men ikke 100.
-        var leapYear = new leapYear();
+        leapYear leapYear = new leapYear();
         assertTrue(leapYear.isLeapYear(4));
         assertFalse(leapYear.isLeapYear(100));
     }
     @Test
     void yeardivisibleByFourHundred() { //Tester at året er delbart med 400.
-        var leapYear = new leapYear();
+        leapYear leapYear = new leapYear();
         assertTrue(leapYear.isLeapYear(400));
         assertTrue(leapYear.isLeapYear(800));
     }
     @Test
     void yeardivisibleByFour() { //Tester at året er delbart med 4.
-        var leapYear = new leapYear();
+        leapYear leapYear = new leapYear();
         assertTrue(leapYear.isLeapYear(4));
         assertTrue(leapYear.isLeapYear(8));
 
     }
     @Test
     void yeardivisibleByFourHundredButNotHundred() { //Tester at året er delbart med 400, men ikke 100.
-        var leapYear = new leapYear();
+        leapYear leapYear = new leapYear();
         assertTrue(leapYear.isLeapYear(400));
         assertFalse(leapYear.isLeapYear(100));
     }
